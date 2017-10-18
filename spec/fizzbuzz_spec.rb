ea923af8725dc6b328aecb170c459d08fea00913
Tabish Raza <tabishraza@gmail.com>
2017-10-18 11:56:30 +0100

    now passes tests for multiples of 3, 5 and both

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -33,5 +33,11 @@ end
 describe "fizzbuzz" do
   it 'returns "buzz" when passed 10' do
     expect(fizzbuzz(10)).to eq "buzz"
-  end  
+  end
+end
+
+describe "fizzbuzz" do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
 end
