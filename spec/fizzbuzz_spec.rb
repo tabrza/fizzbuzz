c6588c356e2e578c4d75e4ee434415d5ae086ff8
Tabish Raza <tabishraza@gmail.com>
2017-10-18 11:47:08 +0100

    Added several tests for multiples of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,21 @@ describe "fizzbuzz" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
 end
+
+describe "fizzbuzz" do
+  it "return 'fizz' when passed 6" do
+    exepct fizzbuzz(6).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 9" do
+    expect fizzbuzz(9).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 12" do
+    expect fizzbuzz(12).to eq "fizz"
+  end
+end
